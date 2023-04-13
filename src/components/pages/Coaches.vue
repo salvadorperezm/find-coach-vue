77288ab07c8ac613239edef40c69eabe5e2da4e5
salvadorperezm <salvadorperezm115@gmail.com>
2023-04-13 10:20:35 -0600

    coaches filter component added

--- src/components/pages/Coaches.vue
+++ src/components/pages/Coaches.vue
@@ -1,7 +1,22 @@
 <template>
-    <h2>Coaches</h2>
+    <div class="page-container">
+        <coaches-filter></coaches-filter>
+    </div>
 </template>
 
 <script>
-export default {}
+import CoachesFilter from "../coaches/CoachesFilter.vue"
+
+export default {
+    components: {
+        CoachesFilter
+    }
+}
 </script>
+
+<style scoped>
+.page-container {
+    max-width: 900px;
+    margin: 0 auto;
+}
+</style>
